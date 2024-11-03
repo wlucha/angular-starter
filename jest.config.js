5a1a2cfcf2dd90b1d828248888ce010e7e80ac38
Wilfried Lucha <wilfried@lucha.at>
2024-11-03 09:13:46 +0100

    build: update dependencies to latest

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   preset: "jest-preset-angular",
   setupFilesAfterEnv: ["<rootDir>/src/setupJest.ts"],
   reporters: ["default", "jest-junit"],
+  transformIgnorePatterns: ["!node_modules/"],
 };
